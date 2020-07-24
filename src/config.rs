2c8f3ebeacdfc5b1bc214b6b6728469f24597144
William Gasper <wkg@williamgasper.com>
2020-07-24 11:26:20 -0500

    using main struct in cfg

--- src/config.rs
+++ src/config.rs
@@ -4,28 +4,38 @@ use serde::Deserialize;
 
 use toml::from_str;
 
-
-
+// TODO: bools should probably be Strings that are then parsed to allow for
+// different options
 #[derive(Deserialize)]
-pub struct Config {
+pub struct MainCfg {
     pub title: Option<String>,
     pub logo: Option<String>,
+    pub notes: Option<bool>,
     pub metadata: Option<String>,
     pub scripts: Option<String>,
     pub q2exports: Option<String>,
     pub notebooks: Option<String>,
 }
 
+#[derive(Deserialize)]
+pub struct Config {
+    pub main: MainCfg,
+}
+
 pub fn get_default_config() -> Config {
-    
-    let config: Config = toml::from_str(r#"
+    let config: Config = toml::from_str(
+        r#"
+    [main]
     title = 'Title placeholder'
     logo = './logo.png'
+    notes = true
     metadata = './metadata.tsv'
     scripts = './scripts'
     q2exports = './exports'
     notebooks = './notebooks'
-    "#).unwrap();
+    "#,
+    )
+    .unwrap();
 
     config
 }
