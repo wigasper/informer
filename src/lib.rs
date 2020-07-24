57d9f6a56df807d811f7499b28ee61844ec97c65
William Gasper <wkg@williamgasper.com>
2020-07-24 11:26:26 -0500

    tests

--- src/lib.rs
+++ src/lib.rs
@@ -1,7 +1,44 @@
+pub mod config;
+pub mod utils;
+
 #[cfg(test)]
 mod tests {
+    use std::path::PathBuf;
+
+    use crate::config::*;
+    use crate::utils::*;
+    //extern crate mditty;
+    use mditty::utils::get_file_extension;
+
     #[test]
     fn it_works() {
         assert_eq!(2 + 2, 4);
+    }
+
+    #[test]
+    fn test_using_mditty() {
+        assert_eq!(get_file_extension(&PathBuf::from("test.rs")), "rs");
+    }
+
+    #[test]
+    fn get_default_config_0() {
+        let cfg = get_default_config();
+
+        assert_eq!(cfg.main.title, Some("Title placeholder".to_owned()));
+        assert_eq!(cfg.main.notes, Some(true));
+        assert_eq!(cfg.main.metadata, Some("./metadata.tsv".to_owned()));
+    }
+
+    #[test]
+    fn find_0() {
+        let its = find(&PathBuf::from("."), &vec!["rs".to_owned()]);
+
+        let expected = vec![
+            PathBuf::from("./src/config.rs"),
+            PathBuf::from("./src/utils.rs"),
+            PathBuf::from("./src/lib.rs"),
+        ];
+
+        assert_eq!(its, expected);
     }
 }
