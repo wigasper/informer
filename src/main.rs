fddf55f62703a8410b20dbff1a2322cfd54b1f79
William Gasper <wkg@williamgasper.com>
2020-07-28 10:46:08 -0500

    main routine

---
+++ src/main.rs
@@ -0,0 +1,46 @@
+extern crate clap;
+
+use informer::config::*;
+use informer::utils::*;
+
+use std::env;
+use std::path::{Path, PathBuf};
+
+use clap::{App, Arg};
+
+fn main() {
+    let current_dir = env::current_dir().unwrap_or_else(|why| {
+        panic!("Could not determine the current working directory: {}", why);
+    });
+
+    let matches = App::new("informer")
+        .version("0.1")
+        .author("William Gasper <wkg@williamgasper.com>")
+        .arg(
+            Arg::with_name("input")
+                .short("i")
+                .long("input")
+                .default_value(current_dir.to_str().unwrap())
+                .long_help(
+                    "Input directory. If not provided, the 
+                current working directory will be used",
+                ),
+        )
+        .arg(
+            Arg::with_name("config")
+                .short("c")
+                .long("config")
+                .long_help("Path to a TOML config file, see sample_config.toml for an example"),
+        )
+        .get_matches();
+
+    let mut cfg: Config = get_default_config();
+
+    if matches.is_present("config") {
+        let cfg_path_str: &str = matches.value_of("config").unwrap();
+        let cfg_path = PathBuf::from(cfg_path_str);
+        cfg = load_config(&cfg_path);
+    }
+
+    init(cfg);
+}
